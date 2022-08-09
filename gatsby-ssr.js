894a51d1ee3be0a546880cb98566d32e5bfcdaea
Travis Henning <henningta@gmail.com>
2022-08-09 19:02:09 -0400

    gatsby-ssr build fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { getInitColorSchemeScript } from '@mui/joy/styles';
-import chroma from 'chroma-js';
 
-exports.onRenderBody = ({ setPreBodyComponents, setHtmlAttributes }) => {
+const onRenderBody = ({ setPreBodyComponents, setHtmlAttributes }) => {
   setPreBodyComponents([getInitColorSchemeScript()]);
-  setHtmlAttributes({ 'initial-color': chroma.random().hex() });
 };
+
+export { onRenderBody };
